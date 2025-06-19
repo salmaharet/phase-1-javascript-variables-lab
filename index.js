2462ef1f496f0256571c29a77b1b4acc4e1aab0e
salmaharet <salmaharet459@gmail.com>
2025-06-19 13:40:09 +0300

    complete my assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+// Use const for variables that should not change
+const companyName = 'Scuber';
+
+// Use let or const for other variables depending on if you’ll change them
+let mostProfitableNeighborhood = 'Chelsea';
+
+let companyCeo = 'Susan Smith';
+
+
